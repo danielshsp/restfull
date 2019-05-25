02df758dfe0f545c67ea967cf670e2682d82ade6
danielshsp <danielshjobs@gmail.com>
2019-05-26 00:50:43 +0300

    mongo docker maven plugin

---
+++ domain/src/main/java/rc/domain/User.java
@@ -0,0 +1,42 @@
+package rc.domain;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONObject;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection = "Users")
+public class User {
+
+    @Id
+    private String id;
+    private Object details;
+   // private Map<String,Object> details = new HashMap();
+
+    public User() {
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Object getDetails() {
+        return details;
+    }
+
+    public void setDetails(Object details) {
+        this.details = details;
+    }
+
+   
+
+   
+}
--- web/src/main/java/rc/web/HotelController.java
+++ web/src/main/java/rc/web/HotelController.java
@@ -1,11 +1,20 @@
 package rc.web;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import rc.domain.Hotel;
+import rc.domain.User;
 import rc.service.HotelService;
-
 import java.util.List;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import rc.service.UserService;
 
 @RestController
 @RequestMapping("/hotels")
@@ -13,6 +22,32 @@ public class HotelController {
 
     @Autowired
     HotelService hotelService;
+    @Autowired
+    UserService userService;
+    
+    
+    @GetMapping("/user/name/{name}")
+    public List<User> findByDetailsNameQuery(@PathVariable String name){
+       return userService.findByDetailsNameQuery(name);
+    }
+    
+
+    @GetMapping("/user/query/{key}/{name}")
+    public List<User> findByDetails(@PathVariable String key ,@PathVariable String name){
+       return userService.findByDetails(key, name);
+      
+    }
+    
+    @GetMapping("/user/all")
+    public List<User> findAllUser(){
+       return userService.findAll();
+    }
+
+    @PostMapping("/user")
+    public void saveUser(@RequestBody User user){
+        //user.setDetails((JSONObject) JSON.parse(userObj));
+        userService.save(user);
+    }
 
     @PutMapping("/update/{id}")
     public void update(@PathVariable final String id ,@RequestBody Hotel hotel){
@@ -28,7 +63,7 @@ public class HotelController {
     @GetMapping("/all")
     public List<Hotel> findAll(){
         return hotelService.findAll();
-    }
+    } 
     @GetMapping("/{id}")
     public Hotel findById(@PathVariable final String id){
         return hotelService.findById(id);
